546e1b9378a3bc11824d4c54504af05a98af80ab
it22554840-ctrl <it22554840@my.sliit.lk>
2026-08-30 17:48:41 +0530

    Delete frontend directory

--- frontend/src/app/(dashboard)/admin/style-submissions/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import StyleReview from "@/components/StyleReview";
-
-export default function AdminStyleSubmissions() {
-  return <StyleReview accent="blue" />;
-}
--- frontend/src/app/(dashboard)/plant/style-submissions/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import StyleReview from "@/components/StyleReview";
-
-export default function PlantStyleSubmissions() {
-  return <StyleReview accent="emerald" />;
-}
